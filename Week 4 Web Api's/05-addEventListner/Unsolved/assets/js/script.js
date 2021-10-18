8b3bcc619490903df564c365ad72773a8ab8691d
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-10-17 20:36:34 -0700

    add event listner week 4 challenge 5

--- Week 4 Web Api's/05-addEventListner/Unsolved/assets/js/script.js
+++ Week 4 Web Api's/05-addEventListner/Unsolved/assets/js/script.js
@@ -1,11 +1,25 @@
 // TODO: Create a variable to hold the count
-
+var count = 0;
 // TODO: Create variables to select the increment button, decrement button and count heading by their respective ids.
-
+var countEl = document.querySelector('#count');
+var decrementEl = document.querySelector('#decrement');
+var incremenntEl = document.querySelector('#increment');
 // TODO: Create a function that displays the current count on the page
-
+function setCounterText() {
+    countEl.textContent = count;
+}
 // TODO: Create an addEventListener that will increment the count on click
 // and calls the function to display the current count
+incremenntEl.addEventListener('click', function () {
+    count++;
+    setCounterText();
+})
 
 // TODO: Create an addEventListener that will decrement the count on click
 // and calls the function to display the current count
+decrementEl.addEventListener('click', function () {
+   if (count > 0 ){
+    count--
+    setCounterText();
+   }
+})
