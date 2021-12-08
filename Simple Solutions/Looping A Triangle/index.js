7cbed4c7e500cc9811f8c7c6ea6eb8cba3845c40
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-12-08 02:30:54 -0800

    simple solutions: Looping a triangle: index.js: created

---
+++ Simple Solutions/Looping A Triangle/index.js
@@ -0,0 +1,12 @@
+// Write a loop that makes seven calls to 
+// console.log to output the following triangle:
+
+// It may be useful to know that you can find 
+// the length of a string by writing .length after it.
+
+for (let line = "#"; line.length < 7; line += "#")
+  console.log(line); 
+
+
+  for (let triangle = "!"; triangle.length < 9; triangle += "!")
+    console.log(triangle);
