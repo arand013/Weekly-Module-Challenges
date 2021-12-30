cf56d908cd27da01593589f93276738f23ca3214
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-12-29 19:51:29 -0800

    Leetcode: coinChange: index.js Started

---
+++ LeetCode Practice/CoinChange/index.js
@@ -0,0 +1,54 @@
+//  Question 
+// You are given an integer array coins representing coins of different 
+// denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. 
+// If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+/* 
+// Example(s)
+1:                                                  2: 
+Input: coins = [1,2,5], amount = 11                 Input: coins = [2], amount = 3
+Output: 3                                           Output: -1
+Explanation: 11 = 5 + 5 + 1
+
+3: 
+Input: coins = [1], amount = 0
+Output: 0
+
+----------- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+// Understanding 
+
+
+
+
+// Diagram 
+
+
+
+//PSUEDOCODE
+
+*/ 
+
+
+// TABULATION APPRAOCH 
+function coinChange(coins, amount) {
+    // Create a cache to store possible coins possiblitites
+    let cache = {};
+
+    //
+    function traverse(coins, amount) {
+        for (let i = 0; i < coins.length; i++) {
+
+        }
+    }
+    
+    
+}
+
+return coinChange()
+
+
