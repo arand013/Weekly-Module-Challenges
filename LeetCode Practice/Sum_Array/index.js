d66a8f59f33296a013db3b4e62cd8373fff62fa7
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-01-12 03:02:39 -0800

    Sum_array: created and solved

---
+++ LeetCode Practice/Sum_Array/index.js
@@ -0,0 +1,30 @@
+// QUESTION: Program to find sum of elements in a given array
+//           sum of elements in a given array 
+
+/* 
+EX: 
+Given an array of integers, find sum of its elements.
+Examples : 
+
+Input : arr[] = {1, 2, 3}
+Output : 6
+1 + 2 + 3 = 6
+
+Input : arr[] = {15, 12, 13, 10}
+Output : 50
+
+*/
+
+
+    // function to return sum of elements  
+    // in an array of size n  
+    function sum(arr) {  
+        let sum = 0; // initialize sum  
+  
+        // Iterate through all elements  
+        // and add them to sum  
+        for (let i = 0; i < arr.length; i++)  
+            sum += arr[i];
+
+            return result; 
+    }
