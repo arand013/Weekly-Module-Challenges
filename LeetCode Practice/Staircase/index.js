bf2e7667b482b34cf5e5034716d223b5a1bfdc7e
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-01-18 04:25:52 -0800

    Leetcode: Staircase: index.js solved

--- LeetCode Practice/Staircase/index.js
+++ LeetCode Practice/Staircase/index.js
@@ -70,3 +70,66 @@ process.stdin.on('data', function(inputStdin) {
     inputString += inputStdin;
 });
 
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+
+/* GOAL: Print a staircase 
+ * INPUT: Value = to size of staircase hieght
+ * OUTPUT: Return, Array of (#) stacked to value given
+ *
+ * Base Case: - numbers = dont' build; string; dont
+ * build. 
+ * Edge Cases: 
+ * Constraints: 0 < n <= 100
+ * The function accepts INTEGER n as parameter.
+ * 
+ * Steps: 
+ * Create function
+ * Init variables
+ * Write a loop to. print 
+ * Return staircase
+ */
+
+// 1: Create staircase funcntion w/ parameter n
+function staircase(n) {
+      // 2: Init outer variables to interpret n into 
+     // a integer of steps
+    let steps = parseInt(n);
+    
+       // 3: : Write a loop to set i = 0 & iterate length of (n) or now (steps) up, (i = machine position)
+    for(let i = 0; i++ < steps;){
+         // 4: Init inner loop variables to interpret (steps - i), 
+         //(spaces(n-i)) & (result)
+        let result = ' ';
+        let spaces = steps - i; 
+        
+        for(let j = spaces; j--;){
+            result += ' ';
+        }
+        for(let j = i; j--;){
+            result += '#'
+        }
+    //4: Return staircase with console.log(result)
+    console.log(result);
+    }
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
