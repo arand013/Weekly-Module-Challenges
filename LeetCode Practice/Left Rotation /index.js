f2108028e78be5e5f8475b23521e173abae7580d
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-01-21 16:02:41 -0800

    leetcode: Left Rotation: index.js started

---
+++ LeetCode Practice/Left Rotation /index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+/* UNDERSTAND 
+ * Design a funtion that can take an array[n] then given a integer 
+ * [d] to rotate this array, have the function now shift all the 
+ * numbers in the given array that many positions to the left; 
+ * 
+ * GOAL: Return an output that contains the  array (INTEGER_ARRAY)
+ * in the shifted postiions. 
+ * 
+ * Base case: array 
+ * Edge case: 
+ * constraints: 
+ * time complexity: O(n) (1 loop to iterate array )
+ * space complecity: O(n) (as data grows so does space equally )
+ * 
+ * 
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+
+function rotateLeft(d, arr) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
