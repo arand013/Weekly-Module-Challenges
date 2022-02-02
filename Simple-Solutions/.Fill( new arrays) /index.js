7250d7f051e7ad347157dda0fc2886fa42fb2c2e
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-02-02 02:44:34 -0800

    Leetcode: Delete Distance Solved

--- Simple-Solutions/.Fill( new arrays) /index.js
+++ Simple-Solutions/.Fill( new arrays) /index.js
@@ -1,3 +1,8 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango", "nice"];
-fruits.fill(" Keep it UP!")
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "nice"];
+// fruits.fill(" Keep it UP!")
+// console.log(fruits); 
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.fill("Kiwi",2,4);
 console.log(fruits); 
