95d1a4d0d1c14d7fae068f5e612ef69503e9b7db
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-02-05 18:21:32 -0800

    Simple-Solutions: Super-Simple-Dev: index.js: section 4 activites solved

--- Simple-Solutions/Super-Simple-Dev/index.js
+++ Simple-Solutions/Super-Simple-Dev/index.js
@@ -6,7 +6,7 @@ let todo = 80;
 console.log(typeof todo);
 
 
-// Create 3 variables: month, dayOfMonth, year. 
+// 1) Create 3 variables: month, dayOfMonth, year. 
 // Use these variables to console.log today's date in 
 // one line in this format: "November 9, 2021"
 
@@ -17,7 +17,7 @@ let year = "2022"
 console.log(month, dayOfMonth, year);
 
 
-// Save the result of 4 + 5 * 3 in a variable 
+// 2) Save the result of 4 + 5 * 3 in a variable 
 // and console.log the result. Do the same with 
 // the result of (4 + 5) * 3. Notice the rules of math 
 // are the same in programming (brackets first, then multiply, then add).
@@ -26,3 +26,13 @@ let maths = (4 + 5 * 3);
 let maths2 = (4 + 5) * 3;
 
 console.log(maths, maths2); 
+
+/* 3) Create a variable age that saves your age (e.g. let age = 25;). 
+*  Create another variable: let message = 'I am ' + age + ' years old'; 
+*  What is the type of the message variable?
+*/
+
+let age = 23 
+let days = ' with 62 days old'
+let message = "I am " + age + ' years' + days; 
+console.log(typeof message);
