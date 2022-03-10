455f19db279022ae06d2d6d5c8d829b232211f14
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-03-10 02:17:49 -0800

    LeetCode Practice

---
+++ LeetCode Practice/Decrease and Conquer/index.js
@@ -0,0 +1,68 @@
+/*
+ *  Homework - Decrease and Conquer
+ *  Utilize the decrease and conquer pattern to solve these problems.
+ */
+
+/*
+ * Number of Ones
+ *
+ * *Given a sorted bit array (values of either 0 or 1), determine the number of 1's in the array.*
+ *
+ * **Parameters**
+ * Input: arr {Array of Integers}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1] --> 8`
+ * `[0, 0, 0] --> 0`
+ * `[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1] --> 7`
+ */
+
+
+function numberOfOnes(arr) {
+    // Variables 
+    let start = 0;
+    let end = arr.length;
+    let midpoint;
+
+
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let start = 0;
+//     let end = arr.length;
+//     let midpoint;
+  
+//     while (start < end) {
+//       midpoint = parseInt(start + parseInt((end - start) / 2));
+//       if (arr[midpoint] === 0) {
+//         start = midpoint + 1;
+//       }
+//       else {
+//         end = midpoint;
+//       }
+//     }
+//     return arr.length - start;
+//   }
+  
