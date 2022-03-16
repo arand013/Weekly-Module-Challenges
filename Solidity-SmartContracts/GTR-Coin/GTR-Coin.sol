b39ded46f7f6fdfa06d13e613575449babe22977
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-03-16 00:38:14 -0700

    Solidity-SmartContract: GTR-Coin: Started

---
+++ Solidity-SmartContracts/GTR-Coin/GTR-Coin.sol
@@ -0,0 +1,13 @@
+pragma solidity >= 0.4.16 <0.9.0;
+
+contract SimpleStorage {
+    uint public storedNumbers; 
+
+    function setAnumber(uint x) public {
+        storedNumbers = x; 
+    }
+
+    function getTheNumberBack() public view returns (uint) {
+        return storedNumbers;
+    }
+}
