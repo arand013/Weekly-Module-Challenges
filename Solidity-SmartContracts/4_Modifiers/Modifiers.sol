1db7b03137840153646fc4d5bbbf9c8b9bff70e0
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-03-16 00:48:45 -0700

    Solidity-SmartContracts: 4_Modifiers: Modifiers: In Progress

--- Solidity-SmartContracts/4_Modifiers/Modifiers.sol
+++ Solidity-SmartContracts/4_Modifiers/Modifiers.sol
@@ -1,5 +1,10 @@
 pragma solidity >=0.7.0 <0.9.0; 
 
 contract ModifierToken {
-  
+    address public minter; 
+    mapping (address => uint) public balances;
+
+    event Sent(address from, address to, uint amount);
+
+
 }
