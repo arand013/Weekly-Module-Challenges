84d9f5226797f628f03f277a6eb7a1a17dae4e6a
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-03-16 00:44:53 -0700

    Solidity-SmartContracts: 3_Data_Types: Data_Types.sol: Started

--- Solidity-SmartContracts/3_Data_Types/Data_Types.sol
+++ Solidity-SmartContracts/3_Data_Types/Data_Types.sol
@@ -1,1 +1,18 @@
 pragma solidity >=0.7.0 <0.9.0;
+
+contract thisGame {
+
+    uint public playerCount = 0;
+    mapping (address => Player) public players;
+
+    enum Level {Novice, Intermediate, Advanced}
+
+    struct Player {
+        address playerAddress;
+        Level playerLevel;
+        string firstName;
+        string lastName;
+    }
+
+
+}
