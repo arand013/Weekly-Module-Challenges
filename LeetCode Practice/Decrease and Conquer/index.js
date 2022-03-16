4b2f77fa093757403a7e8dc8f95117ba728b9f4d
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-03-15 23:31:43 -0700

    LeetCode Practice: Decrease and Conquer: question 6: Setup

--- LeetCode Practice/Decrease and Conquer/index.js
+++ LeetCode Practice/Decrease and Conquer/index.js
@@ -246,3 +246,41 @@ function rotatedArraySearch(nums, target) {
     // YOUR WORK HERE
   }
   
+
+  
+  
+
+
+
+////////////////////////////////////////////////////////////
+//              QUESTION 6
+
+
+
+/*
+ * ## Multiplication Using Russian Peasant [Extra Credit]
+ *
+ * *Given two positive integers, return its product using Russian Peasant method of multiplication*
+ *
+ * Read up on how to apply the Russian Peasant method [here](https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication). It is also referred to as the Egyptian multiplication.
+ *
+ * **Parameters**
+ * Input: a {Integer}
+ * Input: b {Integer}
+ * Output: {Integer}
+ *
+ * **Constraints**
+ * Assume a <= b, and the value of a is N.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ *
+ * **Examples**
+ * `487, 734 --> 357458`
+ * `846, 908--> 768168`
+ */
+
+
+  
+  
+  
