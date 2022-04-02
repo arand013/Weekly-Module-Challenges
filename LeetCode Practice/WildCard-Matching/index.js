8bb0d04b1079bfce8696f49257b81201b6f51efe
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-04-01 17:41:09 -0700

    Leetcode: Practice: WildCard-Matchin: index.js: created startedp

--- LeetCode Practice/WildCard-Matching/index.js
+++ LeetCode Practice/WildCard-Matching/index.js
@@ -39,3 +39,28 @@
 // s contains only lowercase English letters.
 // p contains only lowercase English letters, '?' or '*'.
 // ------------------------------ 
+
+
+
+              // ------------------------------ //
+                        // START CODE //
+
+
+// UNDERSTAND
+// - This is a dynamic programming question Involving ( RECURSION )
+// -  Because this question ask is to compate two sets fo stings 
+// - Then return a " ? " or " * " if a certianf criteria is met 
+// - OR return False if on criteria is met
+
+// - IF to understand the question then find out if it is RECURSIO / DYNAMIC programming by 
+// Noting is there a possibility for Bottomo UP approach. 
+// IF yes the you can also you TOP to BOTTOM
+
+
+// DIAGRAM
+
+
+
+// PSUEDOCODE
+
+// CODE
