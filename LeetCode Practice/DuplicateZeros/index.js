597b2ccf8b23a2a9aca2f1583f5b42a5a0e5b6e0
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-04-11 00:03:44 -0700

    Leetcode: Duplicate Zeros: code Outo Practice 4/8/22

--- LeetCode Practice/DuplicateZeros/index.js
+++ LeetCode Practice/DuplicateZeros/index.js
@@ -5,7 +5,7 @@ Given a fixed-length integer array, arr,
 
 */
 
-
+//Quadtratic Solution
 function duplicateZeros(arr) {
     let initialLength = arr.length;
 
@@ -28,8 +28,36 @@ function duplicateZeros(arr) {
     }
 };
 
+//Linear Solution
+function duplicateZerosLinear(arr) {
+    let initialLength = arr.length;
+    let result = [];
+    let i = 0; 
 
+    while (i < arr.length) {
+        result.push(arr[i]);
 
+        if (arr.length === initialLength) {
+            break;
+        } 
+        
+
+        if (arr[i] === 0) {
+            result.push(0);
+            if (result.length === initiaLength){
+                break;
+            }
+        }
+        i += 1;
+    }
+    console.log("Result: ", result); 
+arr = result("ARR: ", arr)
+};
+
+console.log("Result: ", result); 
+for (let i = 0; i <arr.length; i++) {
+    arr[i] = result[i]
+}
 
 ///////////////////////////////////////////////////////// ///
 // QUESTION 2 - Dominoes
@@ -64,10 +92,57 @@ Observation:
              -if top item doesn't match selectedTop
                 -if top item matched selectedTop, flips++, i++
                 -otherwise, terminat iteration
+    -if termination is 2, return -1 
+    -chppse minimum of flips
+    -if minimum of flips is greater than half of the length of the arrays
 */
 
 
 
-function minDominoRotations(tops, bottoms) { 
+function minDominoRotations(tops, bots) { 
+    let selectedTop = tops[0];
+    let selectedBot = bots[0];
+    let topFlips = 0;
+    let botFlips = 0;
+    let terminations = 0;
+
+
+    for (let i = 0; i < tops.length; i++){
+
+        if(tops[i] !== selectedTop) {
+
+            if(bots[i] === selectedTop) {
+                topFlips++
+            } else {
+                terminations++;
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < bots.length; i++){
+
+        if(bots[i] !== selectedBot) {
+
+            if(tops[i] === selectedBot) {
+                botFlips++
+            } else {
+                terminations++;
+                break;
+            }
+        }
+    }
+
+    if (termination === 2) {
+        return -1;
+    }
+
+    if(termination === 1){
+        topFlips = Math.max(topFlips, botFlips);
+        botFlips = Math.min(topFlips, botFlips);
+    }
+    let minFlip = Math.min(topFlips, botFlips);
+
+    return Math.min(minFlip, tops.length - minFlip);
 
 }
