1d55aa1b0245155b5fc0407c45682a2446353758
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-05-06 03:23:36 -0700

    Week 8 OOP: 03 Algorithms: 02 Is-Unique Test and Function solved

--- Week 8 OOP/03-Algorithms copy/03-is-anagram/Unsolved/is-anagram.js
+++ Week 8 OOP/03-Algorithms copy/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,11 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {;
+// Variables 
+strA = strA.split(' ').sort().join(' ');
+strB = strB.split(' ').sort().join(' ');
+
+let result =  strA === strB;
+return result 
+};
