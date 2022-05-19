1e2e53ef14d47802ae5cfc87e1b0ae0fb7660e01
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-05-18 18:49:56 -0700

    LeetCode: Practice: CorrectABinaryTreeh: index.js: Setup

--- LeetCode Practice/CorrectABinaryTree/index.js
+++ LeetCode Practice/CorrectABinaryTree/index.js
@@ -43,6 +43,34 @@ CONSTRAINTS:
 -fromNode.right is null in the initial tree from the test data.
 */
 
+//NOTES: 
+/* UNDERSTANDING THE QUESTION
+You have a binary tree with a small defect. 
+There is exactly one invalid node where its right child incorrectly points 
+to another node at the same depth but to the invalid node’s right.
+
+
+-Given the root of the binary tree with this defect, 
+root, return the root of the binary tree after removing
+this invalid node and every node underneath it
+(minus the node it incorrectly points to).
+
+The idea is to traverse the BFS sequence. (Breath First Search)
+Because the error is the right pointer of a certain node, 
+our level order traversal will change a little.
+
+For each layer of nodes, we traverse from right to left. 
+When traversing, we put the left and right children of the current node
+into the hashset.
+
+When traversing, it is also tested whether the right child of 
+the left/right child already exists in the hashset.
+
+-If it exists, it means that the right pointer of the left/right 
+child points to a node in the current layer.
+-We can remove the child who points to the wrong one.
+
+*/
 
 
 // ANSWER
