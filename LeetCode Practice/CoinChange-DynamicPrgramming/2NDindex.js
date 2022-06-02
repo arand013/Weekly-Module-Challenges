61677459463fa1d6f22d3a1a7a10f37bd87c56d0
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-06-02 14:46:25 -0700

    Leetcode Practice: CoinChange-Dynamic Programming: 2NDindex.js: created and setup

---
+++ LeetCode Practice/CoinChange-DynamicPrgramming/2NDindex.js
@@ -0,0 +1,44 @@
+//  Question 
+// You are given an integer array coins representing coins of different 
+// denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. 
+// If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+/* 
+// Example(s)
+1:                                                  2: 
+Input: coins = [1,2,5], amount = 11                 Input: coins = [2], amount = 3
+Output: 3                                           Output: -1
+Explanation: 11 = 5 + 5 + 1
+
+3: 
+Input: coins = [1], amount = 0
+Output: 0
+
+// LINK * https://leetcode.com/problems/coin-change/
+
+----------- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+// Understanding 
+1) Reverse Top Down Solutiom
+    a) Find recursive relationships [ involves subtrating amount - coins]
+                                    [track # of coins ]
+    b) Find base cases [When do we need to stop]
+                       [amount = 0] Valid Path / Save or Return # of coins
+                       [amount < 1] INVALID Path / DONT Save or Return # of coins
+                       [i = coins.length - 1 ] Stop Loop / End of Loop
+
+2) 
+
+
+
+// Diagram 
+
+
+
+//PSUEDOCODE
+
+*/
