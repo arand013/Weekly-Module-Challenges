f2ba8a3babb9286b27b7e4945f7c9097f41bc016
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-07-12 23:38:36 -0700

    Javascript Fundamentals: 10-ShoppingList: index.js: challenge question setup and started

---
+++ Javascript-Fundamentals/10-ShoppingList/index.js
@@ -0,0 +1,31 @@
+// CHALLENGE QUESTION
+
+/*
+Create a shopping list in the variable myList. 
+The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+
+*/
+
+
+
+//  TIPS & ADVICE
+
+
+/*
+(1) Every sentence(.) reveals more about how to solve the question.
+(2) Begin by creating a console.log of the given to see and view what is happening as you progress. 
+
+*/ 
+
+// GIVEN
+const myList = [];
+
+// SOLUTION
+console.log(myList);
+
+
