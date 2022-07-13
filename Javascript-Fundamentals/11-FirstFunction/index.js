0dddeb369bacf5c94a713adc4d698bc16fc4f138
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-07-13 01:20:23 -0700

    Javascript Fundamentals: 11-FirstFunction: index.js: challenge question sovled

---
+++ Javascript-Fundamentals/11-FirstFunction/index.js
@@ -0,0 +1,40 @@
+// CHALLENGE QUESTION
+
+/*
+-- Write Reusable JavaScript with Functions --
+In JavaScript, we can divide up our code into reusable parts called functions.
+
+Here's an example of a function:
+
+**************************
+function functionName() {
+  console.log("Hello World");
+}
+**************************
+
+You can call or invoke this function by using its name 
+followed by parentheses, like this: functionName(); Each time the function is called it will print out 
+the message Hello World on the dev console. 
+All of the code between the curly braces will be executed every time the function is called.
+
+Create a function called "reusableFunction" which prints the string "Hi World" to the dev console.
+Call the function.
+*/
+
+
+//  TIPS & ADVICE
+
+/*
+(1) Every sentence(.) reveals more about how to solve the question.
+(2) Begin by creating a console.log of the given to see and view what is happening as you progress. 
+
+*/ 
+
+// SOLUTION
+
+function reusableFunction() {
+    console.log("Hi World");
+  }
+   
+  reusableFunction(); 
+
