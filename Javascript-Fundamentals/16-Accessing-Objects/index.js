c073740cfa147fee2bca7916cb3e6061775f5196
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-07-29 00:11:38 -0700

    Javascript-Fundamentals: Build Javascript Objects: index.js: solved question

--- Javascript-Fundamentals/16-Accessing-Objects/index.js
+++ Javascript-Fundamentals/16-Accessing-Objects/index.js
@@ -38,4 +38,6 @@ const testObj = {
   };
   
   // Only change code below this line
+  const hatValue = testObj.hat      // Change this line
+  const shirtValue = testObj.shirt    // Change this line
 
