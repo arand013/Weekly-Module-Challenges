6df69e6c437606104a687eb490c6e6407ac608ca
Alexander Aranda <alexandermarkets1@gmail.com>
2023-01-27 21:53:38 -0500

    2023-WeeklyWork: 21DayCodeChallenge: day1.js : solved

--- 2023-WeeklyWork/21DayCodeChallenge/day1.js
+++ 2023-WeeklyWork/21DayCodeChallenge/day1.js
@@ -37,29 +37,39 @@ Constraints:
 
 /*----------------------- SOLUTION  ----------------------  */
 
-function deleteMiddleNode(head) {
+  function deleteMiddleNode(head) {
+  // Many linked list problems that modify the list
+  // can be made simpler by temporarily inserting
+  // an extra node as the head.
+  let tempHead = new LLNode(Infinity, head);
 
 
-    // Create function with param head that is a given linked list or Array 
+  // This solution will use a slow/fast pattern. In this
+  // case the fast will move twice as fast as the slow.
+  let slow = tempHead;
+  let fast = head?.next;
 
 
-    // start by creating a variable to hold the linked list
-
-    let result = []; 
-
-    // create a loop to iterate the list and find the middle or the 2nd half and pop either the middle node 
-    // or the 1st node of the 2nd ha;f of the list and
-
-        if ( head.length % 2 === 0 ){ 
-           return  head.length / 2 === 0 ? result.push(result)
-        } else if ( head.length % 2 === 1) { 
-           return head.length / 2 === 1 ? result.push(result)
-        }
-        return result 
-    // return the variable as a new array or list 
-    return result 
+  while (fast) {
+    // Slow moves ahead one.
+    slow = slow.next;
+    // Fast moves ahead two.
+    fast = fast.next?.next;
   }
 
+
+  // Now that fast has moved off the end (now is null)
+  // slow is at the halfway point.
+
+
+  // update the slow pointer's next to skip the node
+  // after it.
+  slow.next = slow.next ? slow.next.next : null;
+
+
+  // Return the real head, not our temporary head.
+  return tempHead.next;
+}
 
 /*------------------- END OF SOLUTION  -------------------  */
 
