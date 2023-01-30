bc3c3a0d54440032ffb5b649e75876ed55db439c
Alexander Aranda <alexandermarkets1@gmail.com>
2023-01-30 01:52:58 -0500

    2023-WeeklyWork: 21DayCodeChallenge: day45js : created

--- 2023-WeeklyWork/21DayCodeChallenge/day5.js
+++ 2023-WeeklyWork/21DayCodeChallenge/day5.js
@@ -1,1 +1,55 @@
-Given a rectangular 2D array of integers, return true if all rows and all columns are monotonically increasing. This means that every successive value along all rows and columns must be AT LEAST as large as what came before.
+
+
+//                       QUESTION
+
+/*
+Given a rectangular 2D array of integers, return true if all 
+rows and all columns are monotonically increasing. 
+This means that every successive value along 
+all rows and columns must be AT LEAST as large as what came before.
+
+*/
+
+
+/*  
+EXAMPLE 1: 
+
+    [[0, 0, 0, 1],
+    [1, 1, 1, 2],
+    [2, 3, 4, 5]]
+
+
+Output:
+    Returns true but this next one returns false.
+
+    [[0, 0, 0, 1],
+    [1, 1, 3, 2],
+    [2, 3, 4, 5]] 
+
+*/
+
+    
+
+/*
+Constraints:    
+
+--    [  ]
+--    [  ]
+
+*/ 
+
+/*----------------------- SOLUTION  ----------------------  */
+
+function isMatrixMonotonic(matrix) {
+    /* your code here */
+   }
+/*------------------- END OF SOLUTION  -------------------  */
+  
+  /* */
+  
+  /*                          FOLLOW UP                        */ 
+  /* 
+  Follow Up: 
+  
+  --------------------------------------------------------------------------
+  */ 
