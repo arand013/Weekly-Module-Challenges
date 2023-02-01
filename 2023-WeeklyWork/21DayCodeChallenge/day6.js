7ed44c1a02b8e8abe96702e50381d8b579af03ed
Alexander Aranda <alexandermarkets1@gmail.com>
2023-02-01 03:07:13 -0500

    2023-WeeklyWork: 21DayCodeChallenge: day6.js: created

--- 2023-WeeklyWork/21DayCodeChallenge/day6.js
+++ 2023-WeeklyWork/21DayCodeChallenge/day6.js
@@ -31,8 +31,36 @@ Constraints:
 
 
 function findInMonotonic(matrix, k) {
-    /* your code here */
+  const nrows = matrix.length;
+  if (nrows === 0) return false;
+
+
+  // Start at a corner that is the end of one dimension
+  // and start of another.
+  let row = nrows - 1;
+  let col = 0;
+  const ncols = matrix[row].length;
+
+
+  while (row >= 0 && col < ncols) {
+    const value = matrix[row][col];
+    if (value === k) {
+      // We found the target value!
+      return true;
+    } else if (value < k) {
+      // If the k is greater than this value, then it HAS
+      // to be at least one column over.
+      col++;
+    } else {
+      // If the k is less than this value, then it HAS
+      // to be at least one row up.
+      row--;
+    }
   }
+
+
+  return false;
+}
 /*------------------- END OF SOLUTION  -------------------  */
   
   /* */
