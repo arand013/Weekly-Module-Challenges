8a8c078f53336c3024c3fe6dd95b60cbdb01e232
Alexander Aranda <alexandermarkets1@gmail.com>
2023-02-01 03:30:47 -0500

    2023-WeeklyWork: 21DayCodeChallenge: day8.js: created

---
+++ 2023-WeeklyWork/21DayCodeChallenge/day8.js
@@ -0,0 +1,59 @@
+
+
+//                       QUESTION
+
+/*
+Given a binary tree, convert this to a doubly linked list in the order of the in-order traversal. 
+This operation should be done in place, not creating any new data structure. You must re-use 
+the node's left pointer as the pointer to the previous node in the list and the right pointer 
+should be the next node in the list.
+
+
+At the end, return a pointer the first node in the list.
+*/
+
+
+/*  
+EXAMPLE 1: 
+
+Otherwise, return false.
+*/
+
+    
+
+/*
+Constraints:    
+
+--    [  ]
+--    [  ]
+
+*/ 
+
+/*----------------------- SOLUTION  ----------------------  */
+
+ /*
+  * You may assume the node class is:
+  * class Node {
+  *   constructor(value, left = null, right = null) {
+  *     this.value = value;
+  *     this.left = left;
+  *     this.right = right;
+  *   }
+  * }
+  */
+ 
+ function tree2list(root) {
+    /* your code here */
+  }
+
+
+/*------------------- END OF SOLUTION  -------------------  */
+    
+    /* */
+    
+    /*                          FOLLOW UP                        */ 
+    /* 
+    Follow Up: 
+    
+    --------------------------------------------------------------------------
+*/ 
