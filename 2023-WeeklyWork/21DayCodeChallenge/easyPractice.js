2748f09e8e8154df96d6138426ad2ab582911493
Alexander Aranda <alexandermarkets1@gmail.com>
2023-02-09 23:59:02 -0500

    easyPractice.js: theGreeting function created;

--- 2023-WeeklyWork/21DayCodeChallenge/easyPractice.js
+++ 2023-WeeklyWork/21DayCodeChallenge/easyPractice.js
@@ -5,20 +5,20 @@
  * Be sure to fix it in the spirit of the code, do not hard code the result.
  */
 
-function troubleshooting() {
-	const a = 1;
-	const b = 1;
+	function troubleshooting() {
+		const a = 1;
+		const b = 1;
 
-	let result;
+		let result;
 
-	// Edit between these lines
-	// =================================
-	result = a + b;
+		// Edit between these lines
+		// =================================
+		result = a + b;
 
-	// =================================
+		// =================================
 
-	return result;
-}
+		return result;
+	}
 console.log(troubleshooting());
 // Do not change this 
 module.exports = troubleshooting;
@@ -32,17 +32,18 @@ module.exports = troubleshooting;
  * The code below tells the browser to ask you for a number
  * then if that number is `6`, it returns `true` otherwise it returns `false`
  * 
- * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ * Change this code so it returns `true` when the number is greater than 
+ * or equal to 10, and false if it is less than 10
  */
 
 
-function numberChecker(number) {
-	if(number >= 10) {
-		return true;
-	} else if (number < 10) {
-		return false;
+	function numberChecker(number) {
+		if(number >= 10) {
+			return true;
+		} else if (number < 10) {
+			return false;
+		}
 	}
-}
 console.log(numberChecker(49));
 
 
@@ -63,14 +64,42 @@ console.log(numberChecker(49));
 * d = the variable 'a' minus 17
 * e = the sum of the previous four variables
 */
-function variables() {
+	function variables() {
 
-let f = 1+8;
-let g = 22 * 3;
-let c = 5 % 4;
-let d = f - 17;
-let e = f + g + c + d;
+	let f = 1+8;
+	let g = 22 * 3;
+	let c = 5 % 4;
+	let d = f - 17;
+	let e = f + g + c + d;
 
-return console.log(f, g, c, d, e);
+	return console.log(f, g, c, d, e);
 }
 variables();
+
+// ===================================================================================================
+// ===================================================================================================
+// ===================================================================================================
+
+
+/*
+4 variables: first name, last name, current year, and birth year.
+a 5th variable (greeting) that is constructed from the previous 4, 
+it should contain a greeting with the person's full name and their age.
+print the greeting with console.log
+*/
+
+	const theGreeting = () => {
+		// Create 4 variables
+		let firstName = 'Alexander'; 
+		let lastName = "Aranda";
+		let currentYear = 2023;
+		let birthYear = 1998;
+
+		// 5th variable (greeting) add all 4 variables
+		const greeting = "Hello my name is " + firstName + " " + lastName + ". It is the year " + currentYear + " and I was born on " + birthYear + "."
+
+
+		// return console.log of 5th
+		return console.log(greeting)
+	}
+theGreeting()
