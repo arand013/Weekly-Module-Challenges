ddbf907f231cd0feab0d532219df2b47235b78e5
Alexander Aranda <alexandermarkets1@gmail.com>
2024-07-14 02:35:56 -0400

    Major Updates: Leetcode: 1346: Check Doubles

---
+++ LeetCode Practice/1346. Check If N and Its Double Exist/index.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        
+        for i in range(len(arr)): 
+            for j in range (i + 1, len(arr)):
+                if((arr[i] == 2 * arr[j]) or (arr[i] == arr[j]/2)):
+                    return True
+        return False
+    
+
+class SolutionWComments:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        # Iterate through each element in the array
+        for i in range(len(arr)): 
+            # Iterate through each element that comes after the current element
+            for j in range (i + 1, len(arr)):
+                # Check if the current element is twice the next element or the next element divided by 2
+                if((arr[i] == 2 * arr[j]) or (arr[i] == arr[j]/2)):
+                    # If a match is found, return True
+                    return True
+        # If no match is found after iterating through the entire array, return False
+        return False
