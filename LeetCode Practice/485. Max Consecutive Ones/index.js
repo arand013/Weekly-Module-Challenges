24c95ef4e5b2c44a7085a25250d1e8d2e266907d
Alexander Aranda <alexandermarkets1@gmail.com>
2024-08-16 00:58:47 -0400

    Leetcode Practice: 485 leetcode solved

--- LeetCode Practice/485. Max Consecutive Ones/index.js
+++ LeetCode Practice/485. Max Consecutive Ones/index.js
@@ -1,2 +1,26 @@
 // https://leetcode.com/problems/max-consecutive-ones/description/
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    
+    // variable count 
+    let count = 0
+    let MaxCount = 0
+
+
+    // loop to count consecutive ones
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] == 1){
+            count++
+            MaxCount = Math.max(MaxCount, count)
+        } else {
+            count = 0
+        }
+    }
+    // result
+    return MaxCount
+    
+};
