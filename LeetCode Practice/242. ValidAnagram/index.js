56bb2eb632aea613b9b98551a789390aa23a0071
Alexander Aranda <alexandermarkets1@gmail.com>
2025-02-16 20:11:07 -0500

    latest update 2.16.2025

---
+++ LeetCode Practice/242. ValidAnagram/index.js
@@ -0,0 +1,16 @@
+
+// Identify the length are the same 
+// Check and coount the ooccurence of each character () Hash Map
+// 1 hashmap for each string 
+
+var isAnagram = function(s, t) {
+
+    if(s.length !== t.length) return false;
+
+    for(i = 0; i < s.length; i++){
+        hash.set(s[i], t[i]);
+
+    }
+       
+    
+};
